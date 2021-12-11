87cff1d37499def1128edb60681f92c50ca13741
VukGr <vuk.grujic.5@hotmail.com>
2021-12-11 20:10:35 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,131 @@
+from collections import deque
+
+HZID = '═'
+VZID = '║'
+HEMPTY = '─'
+VEMPTY = '│'
+MID = '┼'
+
+P1 = 'X'
+P2 = 'O'
+
+WIDTH = 20
+HEIGHT = 10
+
+class Game:
+    v_walls = []
+    h_walls = []
+
+    x_pos = {(0,0), (1,1)}
+    o_pos = {(10,10), (8,8)}
+
+    def __init__(self, w, h):
+        self.v_walls = [[] for _ in range(w * 2)]
+        self.h_walls = [[] for _ in range(h)]
+
+        self.v_walls[0] = [0,2,4]
+        self.v_walls[1] = [0,2,4]
+        self.h_walls[0] = [0,3]
+        pass
+
+    def interior(self):
+        for y in range(HEIGHT):
+            v_wall_row = deque(self.v_walls[y])
+            # Vert zidovi i igraci
+            for x in range(WIDTH):
+                # Igraci
+                if (x,y) in self.x_pos:
+                    print(P1, end="")
+                elif (x,y) in self.o_pos:
+                    print(P2, end="")
+                else:
+                    print(" ", end="")
+                # Vert zidovi
+                if x < WIDTH-1:
+                    if len(v_wall_row) > 0 and x == v_wall_row[0]:
+                        v_wall_row.popleft()
+                        print(VZID, end="")
+                    else:
+                        print(VEMPTY, end="")
+            yield
+            # Horizontalni zidovi
+            skip_next = False
+            h_wall_row = deque(self.h_walls[y])
+            if y < HEIGHT-1:
+                for x in range(WIDTH):
+                    if skip_next:
+                        print(HZID, end="")
+                        skip_next = False
+                    else:
+                        if len(h_wall_row) > 0 and x == h_wall_row[0]:
+                            h_wall_row.popleft()
+                            skip_next = True
+                            print(HZID, end="")
+                        else:
+                            print(HEMPTY, end="")
+                    if(x != WIDTH-1):
+                        print(MID,end="")
+            yield
+
+    def drawTerminal(self):
+        interior = self.interior()
+        marks = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        # 12356...H    
+        print(" ", end=" ") # namesta poziciju za 12356...H, tj pomera ga za 3 da se preklopi
+        for y in range(WIDTH):
+                print(marks[y], end = " ") # stampa ══════════════════ za gornji deo
+        print() # prelazak u newline zbog ovo iznad
+
+        #╔══╗
+        print(" ╔", end="") # levi cosak
+        for y in range(WIDTH-1):
+            print( '═', end="╦") 
+        print("═", end="╗") # desni cosak
+
+        # 12356...H + ║ + matrica + ║ 
+        pom = 0
+        for x in range(HEIGHT*2-1):
+            print() # spusta u novu liniju jer koristimo svuda end=
+            if ( x % 2 == 0):
+                print(marks[x-pom], end="")
+                print( '║', end="")
+                pom += 1
+            else:
+                print(' ', end="")
+                print( '╠', end="")
+            next(interior)
+            if ( x % 2 == 0):
+                print( '║', end="")
+            else:
+                print( '╣', end="")
+
+        #╚══╝
+        print() # spusta u novu liniju jer koristimo svuda end=
+        print(" ╚", end="") # levi cosak 
+        for y in range(WIDTH-1):
+            print( '═', end="╩") # stampa ══════════════════ za donji deo
+        print( '═', end="╝") # desni cosak
+
+    def placeHorizontalWall(self, posY, posX):
+        if(posX < WIDTH-1 and posX not in self.h_walls[posY]):
+            self.h_walls[posY].append(posX)
+            self.h_walls[posY].sort()
+
+    def placeVerticalWall(self, posY, posX):
+        if(posY < HEIGHT-1 and posX not in self.v_walls[posY]):
+            self.v_walls[posY].append(posX)
+            self.v_walls[posY].sort()
+            self.v_walls[posY+1].append(posX)
+            self.v_walls[posY+1].sort()
+
+
+g = Game(WIDTH, HEIGHT)
+g.placeHorizontalWall(1, 3)
+g.placeHorizontalWall(1, 1)
+g.placeVerticalWall(1, 1)
+g.placeVerticalWall(1, 3)
+interior = g.interior()
+
+for _ in range(HEIGHT*2):
+    next(interior)
+    print()
