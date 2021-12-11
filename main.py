ec0ca71dd9a0576873999da90fff57951c2565bf
dusanlazarevic-dev <73256535+dusanlazarevic-dev@users.noreply.github.com>
2021-12-11 21:22:34 +0100

    testbee

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ class Game:
             self.v_walls[posY+1].sort()
 
 
-# testcomsds
+# tdestcomsdss
 def parseState(startStateString):
     elements = startStateString.split("|")
     return Game()
