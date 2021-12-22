3b3e504c509fadebf286b3a36e33c5f2d0676509
VukGr <vuk.grujic.5@hotmail.com>
2021-12-22 19:33:09 +0100

    Added graph changes for moving players.

--- GameState.py
+++ GameState.py
@@ -1,5 +1,6 @@
 from collections import deque
 from Util import Point, sign
+from Game import marks
 
 # Temporary, hehe
 def allMoves():
@@ -12,16 +13,22 @@ def straightMoves():
 def diagonalMoves():
     return [Point(+1,+1),Point(-1,+1),Point(-1,-1),Point(+1,-1)]
 
+def halfMoves():
+    return [Point(+1,0),Point(-1,0),Point(0,+1),Point(0,-1)]
+
+def pointToStr(p):
+    return f"({marks[p.x]},{marks[p.y]})"
+
 class GameNode:
     def __init__(self, pos, moves=[]):
         self.pos = Point(*pos)
         self.moves = moves
 
     def __repr__(self):
-        return str((self.pos, self.moves))
+        return f"Node(pos={pointToStr(self.pos)}, moves={[(m.x,m.y) for m in self.moves]})"
 
 class GameState:
-    def __init__(self, width, height, x_pos, o_pos):
+    def __init__(self, width, height, x_pos, o_pos, human_player):
         # Samo se koristi ako je prazan state!
         # tj nema zidova
         def genMoves(src):
@@ -39,6 +46,8 @@ class GameState:
         self.o_pos = o_pos
         self.v_walls = [[] for _ in range(self.width)]
         self.h_walls = [[] for _ in range(self.height)]
+        self.playing = 'X'
+        self.human_player = human_player
         self.graph = [[GameNode(Point(x, y), genMoves(Point(x,y)))
                        for x in range(width)]
                       for y in range(height)]
@@ -100,10 +109,10 @@ class GameState:
             for y in [pos.y-1, pos.y, pos.y+1, pos.y+2]:
                 for x in [pos.x, pos.x+1]:
                     if self.inBounds((x,y)):
-                        invalidMove = (0,+2) if y <= pos.y else (0,-2)
+                        invalidMoves = {(0,+2),(0,+1)} if y <= pos.y else {(0,-2),(0,-1)}
                         self.graph[y][x].moves = [
                             move for move in self.graph[y][x].moves
-                            if move != invalidMove]
+                            if move not in invalidMoves]
             # Diagonal affected for y,y+1 for x-1, x, x+1, x+2
             for y in [pos.y, pos.y+1]:
                 for x in [pos.x-1, pos.x, pos.x+1, pos.x+2]:
@@ -118,10 +127,10 @@ class GameState:
             for x in [pos.x-1, pos.x, pos.x+1, pos.x+2]:
                 for y in [pos.y, pos.y+1]:
                     if self.inBounds((x,y)):
-                        invalidMove = (+2,0) if x <= pos.x else (-2,0)
+                        invalidMoves = {(+2,0),(+1,0)} if x <= pos.x else {(-2,0),(-1,0)}
                         self.graph[y][x].moves = [
                             move for move in self.graph[y][x].moves
-                            if move != invalidMove]
+                            if move != invalidMoves]
             # Diagonal affected for x,x+1 for y-1, y, y+1, y+2
             for x in [pos.x, pos.x+1]:
                 for y in [pos.y-1, pos.y, pos.y+1, pos.y+2]:
@@ -155,5 +164,50 @@ class GameState:
                     return True
         return False
 
+    def movePiece(self, piece, new_position):
+        def fixNewSpace(src):
+            # Replace straight moves with half ones
+            nodes = [src+m for m in straightMoves() if self.inBounds(src+m)]
+            for n in nodes:
+                rel = src - n
+                halfRel = Point(rel.x//2, rel.y//2)
+                self.graph[n.y][n.x].moves = [m for m in self.graph[n.y][n.x].moves
+                                              if m != rel]
+                self.graph[n.y][n.x].moves.append(halfRel)
+            # Remove diagonal moves
+            nodes = [src+m for m in diagonalMoves() if self.inBounds(src+m)]
+            for n in nodes:
+                rel = src - n
+                self.graph[n.y][n.x].moves = [m for m in self.graph[n.y][n.x].moves
+                                              if m != rel]
 
-gs = GameState(50, 50, [(0,0)], [(0,2)])
+        def fixPrevSpace(src):
+            # Replace half moves with proper ones
+            nodes = [src+m for m in straightMoves() if self.inBounds(src+m)]
+            for n in nodes:
+                rel = src - n
+                halfRel = Point(rel.x//2, rel.y//2)
+                self.graph[n.y][n.x].moves = [m for m in self.graph[n.y][n.x].moves
+                                              if m != halfRel]
+                self.graph[n.y][n.x].moves.append(rel)
+            # Re-add diagonal moves
+            nodes = [src+m for m in diagonalMoves() if self.inBounds(src+m)]
+            for n in nodes:
+                rel = src - n
+                self.graph[n.y][n.x].moves.append(rel)
+
+        player_pos = self.x_pos if self.playing == 'X' else self.o_pos
+        piece_pos = Point(*player_pos[piece])
+        new_position = Point(*new_position)
+        move = new_position - piece_pos
+
+        if move in self.graph[piece_pos.y][piece_pos.x].moves:
+            fixPrevSpace(piece_pos)
+            fixNewSpace(new_position)
+            player_pos[piece] = new_position
+            return True
+        else:
+            print("Invalid move")
+            return False
+
+gs = GameState(50, 50, [(0,0)], [(0,2)], 'X')
